aaab2aefbd303f9480b36741dfd6afdf9daaf89b
roman-cf <roman.springer@outlook.com>
2019-12-07 17:19:43 +0100

    -

--- src/citybike/Rent.java
+++ src/citybike/Rent.java
@@ -12,7 +12,7 @@ public class Rent {
         RentEnd = null;
     }
 
-    public void setRentEnd(LocalDateTime rentEnd) {
-        RentEnd = rentEnd;
+    public void rentEnd() {
+        RentEnd = LocalDateTime.now();
     }
 }
